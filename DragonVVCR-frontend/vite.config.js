521fa8127176a96b4492e964c135cf93c769624f
haoge333 <liuhaohgzzz@163.com>
2025-12-27 01:41:01 +0800

    优化界面显示

--- DragonVVCR-frontend/vite.config.js
+++ DragonVVCR-frontend/vite.config.js
@@ -15,4 +15,13 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    proxy: {
+      '/dnvvcr': {
+        target: 'http://localhost:8088',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/dnvvcr/, '/dnvvcr')
+      }
+    }
+  }
 })
